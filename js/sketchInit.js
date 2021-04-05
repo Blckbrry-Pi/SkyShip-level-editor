5b5485c2c512762023f0d8e4bc0bf0434d1eace7
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-04-04 20:59:38 -0400

    Make proof of concept div of sketches

--- js/sketchInit.js
+++ js/sketchInit.js
@@ -1,13 +1,47 @@
 import { initStars, starryBackground } from 'https://blckbrry-pi.github.io/SkyShip/js/extraFunctions/backgroundStars.js';
 
+const sketchHeight = 100;
+const containerPos = createVector(100, 100);
+const containerSize = createVector(200, 250);
+
+function s(sketch) {
+
+  let x = 100;
+  let y = 100;
+
+  sketch.setup = () => {
+    sketch.createCanvas(200, 100);
+  };
+
+  sketch.draw = () => {
+    sketch.stroke(200, 0, 0);
+    sketch.strokeWeight(3);
+    sketch.fill(255);
+    sketch.rect(0, 0, sketch.width, sketch.height)
+    sketch.noStroke();
+    sketch.fill(0);
+    sketch.textAlign(LEFT, TOP);
+    sketch.text(sketch.mouseX + ", " + sketch.mouseY, 3, 3);
+  };
+};
+
 export function preload() {
   
 }
 export function setup() {
   createCanvas(windowWidth, windowHeight);
-  initStars(100);
+  initStars(width * height / 320);
+
+  let div = createDiv('');
+  div.style('overflow', 'scroll');
+  div.style('max-height', containerSize.y + 'px');
+  div.position(containerPos.x, containerPos.y);
+
+  for (let i = 0; i < 10; i++) div.elt.appendChild(new p5(s).canvas);
 }
 export function draw() {
   starryBackground(false);
-  rect(100, 100, 200, 200);
+  stroke(0, 255, 0);
+  strokeWeight(4);
+  rect(containerPos.x, containerPos.y, containerSize.x, containerSize.y);
 }
