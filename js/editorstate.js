0de1308415a224f68b256a8ca4715f764a7b1547
Anthony Li <me@anli.dev>
2021-04-05 12:27:46 -0400

    Add EditorState.setState

--- js/editorstate.js
+++ js/editorstate.js
@@ -10,7 +10,31 @@ import { Runner } from 'https://blckbrry-pi.github.io/SkyShip/js/classes/runner.
 export class EditorState {
   constructor() {
     /** Name of the current state. */
-    this.stateName = "";
+    this.stateName = "selectExisting";
+
+    /**
+     * Type of selected object.
+     * @type {"attractors" | "zippers" | "obstacles" | "finishLine" | "runner" | null}
+     * **/
+    this.objectType = null;
+
+    /**
+     * Index of selected attractor, zipper, or obstacle.
+     * @type {number | null}
+     */
+    this.selectedIndex = null;
+  }
+
+  /**
+   * Updates the current editor state.
+   * @param {string} stateName
+   * @param {"attractors" | "zippers" | "obstacles" | "finishLine" | "runner" | null} objectType
+   * @param {number | null} selectedIndex
+   */
+  setState(stateName, objectType = null, selectedIndex = null) {
+    this.stateName = stateName;
+    this.objectType = objectType;
+    this.selectedIndex = selectedIndex;
   }
 }
 
