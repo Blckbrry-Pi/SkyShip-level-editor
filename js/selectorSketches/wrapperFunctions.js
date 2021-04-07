223458900474694d4d25d9668d838f77dc79dc51
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-04-07 10:29:07 -0400

    Remove extraneous files

--- js/selectorSketches/wrapperFunctions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Used to pass "onClick" and "available" functions into sketch-creating function for creating a instance mode sketch. 
- * @param {function} mainFunction
- * @param {...function} callbacks
- * **/
-export function passCallbacks(mainFunction, ...callbacks) {
-    return (sketch) => {mainFunction(sketch, ...callbacks)};
-}
