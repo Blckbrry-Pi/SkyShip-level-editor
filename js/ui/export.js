0a8e5fc017df8ce67f83efd57040f4e30121fc74
Anthony Li <me@anli.dev>
2021-04-07 12:56:39 -0400

    Add level importing

--- js/ui/export.js
+++ js/ui/export.js
@@ -1,3 +1,4 @@
+import { JSON2Level } from 'https://blckbrry-pi.github.io/SkyShip/js/extraFunctions/levels.js';
 import { exportLevel } from '../utils.js';
 import { Modal } from './modal.js';
 
@@ -7,22 +8,50 @@ export class ExportModal extends Modal {
    * @param {Level} level
    */
   constructor(element, level) {
-    super(element, {copy: () => {
-      this.copy();
-      return false;
-    }, close: () => true});
+    super(element, {
+      copy: () => {
+        this.copy();
+        return false;
+      }, 
+      cancel: () => {
+        return true;
+      },
+      import: () => {
+        if (this.isDirty) {
+          try {
+            this.import();
+          } catch (/** @type {Error} */ e) {
+            alert(`Failed to import level: ${e.message}`);
+            return false;
+          }
+        }
+        return true;
+      }
+    });
 
     this.level = level;
     
     this.textarea = element.getElementsByTagName("textarea")[0];
+    
+    this.textarea.addEventListener("change", _ => {
+      this.isDirty = true;
+    });
   }
 
   open() {
     this.textarea.value = exportLevel(this.level);
+    this.isDirty = false;
     super.open();
   }
 
+  import() {
+    this.level = JSON2Level(this.textarea.value);
+    
+    // TODO: Resolve global state weirdness
+    level = this.level;
+  }
+
   copy() {
-    navigator.clipboard.writeText(exportLevel(this.level));
+    navigator.clipboard.writeText(this.textarea.value);
   }
 }
